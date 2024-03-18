2a67c671a0032dedafaf60935cd00d0adf3623a7
DESKTOP-4GPR761\HashTable <Rayato159@gmail.com>
2024-03-19 01:07:24 +0700

    :bug: fix paginate

--- domains/itemShop/model/item.go
+++ domains/itemShop/model/item.go
@@ -17,7 +17,7 @@ type (
 
 	Paginate struct {
 		Page int64 `query:"page" validate:"required,min=1"`
-		Size int64 `query:"size" validate:"required,min=1 max=20"`
+		Size int64 `query:"size" validate:"required,min=1,max=20"`
 	}
 
 	ItemResult struct {
