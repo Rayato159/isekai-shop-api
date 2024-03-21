fc058955f59536c2664a5be5072ae2b6288e9791
DESKTOP-4GPR761\HashTable <Rayato159@gmail.com>
2024-03-22 00:56:45 +0700

    :package: refactor

--- entities/inventory.go
+++ entities/inventory.go
@@ -2,12 +2,10 @@ package entity
 
 import "time"
 
-type (
-	Inventory struct {
-		ID        uint64    `gorm:"primaryKey;autoIncrement;"`
-		PlayerID  string    `gorm:"type:varchar(64);not null;"`
-		ItemID    uint64    `gorm:"type:bigint;not null;"`
-		IsDeleted bool      `gorm:"not null;default:false;"`
-		CreatedAt time.Time `gorm:"not null;autoCreateTime;"`
-	}
-)
+type Inventory struct {
+	ID        uint64    `gorm:"primaryKey;autoIncrement;"`
+	PlayerID  string    `gorm:"type:varchar(64);not null;"`
+	ItemID    uint64    `gorm:"type:bigint;not null;"`
+	IsDeleted bool      `gorm:"not null;default:false;"`
+	CreatedAt time.Time `gorm:"not null;autoCreateTime;"`
+}
