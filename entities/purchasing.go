28bcbcaed73616c4c71cd1268b95fdd567c5fbd9
DESKTOP-4GPR761\HashTable <Rayato159@gmail.com>
2024-03-22 00:59:16 +0700

    :package: refactor

--- entities/purchasing.go
+++ entities/purchasing.go
@@ -4,16 +4,14 @@ import (
 	"time"
 )
 
-type (
-	PurchaseHistory struct {
-		ID              uint64    `gorm:"primaryKey;autoIncrement;"`
-		PlayerID        string    `gorm:"type:varchar(64);not null;"`
-		ItemID          uint64    `gorm:"type:bigint;not null;"`
-		ItemName        string    `gorm:"type:varchar(64);not null;"`
-		ItemDescription string    `gorm:"type:varchar(128);not null;"`
-		ItemPrice       uint      `gorm:"not null;"`
-		ItemPicture     string    `gorm:"type:varchar(128);not null;"`
-		Quantity        uint      `gorm:"not null;"`
-		CreatedAt       time.Time `gorm:"not null;autoCreateTime;"`
-	}
-)
+type PurchaseHistory struct {
+	ID              uint64    `gorm:"primaryKey;autoIncrement;"`
+	PlayerID        string    `gorm:"type:varchar(64);not null;"`
+	ItemID          uint64    `gorm:"type:bigint;not null;"`
+	ItemName        string    `gorm:"type:varchar(64);not null;"`
+	ItemDescription string    `gorm:"type:varchar(128);not null;"`
+	ItemPrice       uint      `gorm:"not null;"`
+	ItemPicture     string    `gorm:"type:varchar(128);not null;"`
+	Quantity        uint      `gorm:"not null;"`
+	CreatedAt       time.Time `gorm:"not null;autoCreateTime;"`
+}
