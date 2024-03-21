a2bd397ec5283c4545ae5399f1941640815c6021
DESKTOP-4GPR761\HashTable <Rayato159@gmail.com>
2024-03-22 00:58:05 +0700

    :package: refactor

--- entities/player.go
+++ entities/player.go
@@ -6,17 +6,15 @@ import (
 	_playerModel "github.com/Rayato159/isekai-shop-api/pkg/player/model"
 )
 
-type (
-	Player struct {
-		ID          string      `gorm:"primaryKey;type:varchar(64);"`
-		Email       string      `gorm:"type:varchar(128);unique;not null;"`
-		Name        string      `gorm:"type:varchar(128);not null;"`
-		Avatar      string      `gorm:"type:varchar(256);not null;default:'';"`
-		Inventories []Inventory `gorm:"foreignKey:PlayerID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-		CreatedAt   time.Time   `gorm:"not null;autoCreateTime;"`
-		UpdatedAt   time.Time   `gorm:"not null;autoUpdateTime;"`
-	}
-)
+type Player struct {
+	ID          string      `gorm:"primaryKey;type:varchar(64);"`
+	Email       string      `gorm:"type:varchar(128);unique;not null;"`
+	Name        string      `gorm:"type:varchar(128);not null;"`
+	Avatar      string      `gorm:"type:varchar(256);not null;default:'';"`
+	Inventories []Inventory `gorm:"foreignKey:PlayerID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	CreatedAt   time.Time   `gorm:"not null;autoCreateTime;"`
+	UpdatedAt   time.Time   `gorm:"not null;autoUpdateTime;"`
+}
 
 func (p *Player) ToPlayerModel() *_playerModel.Player {
 	return &_playerModel.Player{
