55d881a883357344a18d956a61a29a550c7222d1
DESKTOP-4GPR761\HashTable <Rayato159@gmail.com>
2024-03-25 22:38:15 +0700

    :package: refactoring

--- pkg/oauth2/model/userInfo.go
+++ pkg/oauth2/model/userInfo.go
@@ -1,15 +1,12 @@
 package model
 
-type (
-	// This struct is provided by Google OAuth2
-	UserInfo struct {
-		ID            string `json:"id"`
-		Email         string `json:"email"`
-		VerifiedEmail bool   `json:"verified_email"`
-		Name          string `json:"name"`
-		GivenName     string `json:"given_name"`
-		FamilyName    string `json:"family_name"`
-		Picture       string `json:"picture"`
-		Locale        string `json:"locale"`
-	}
-)
+type UserInfo struct {
+	ID            string `json:"id"`
+	Email         string `json:"email"`
+	VerifiedEmail bool   `json:"verified_email"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Picture       string `json:"picture"`
+	Locale        string `json:"locale"`
+}
