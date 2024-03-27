a89ac372e31aa9386eccc8f3ce145054780754ce
DESKTOP-4GPR761\HashTable <Rayato159@gmail.com>
2024-03-28 00:19:11 +0700

    :package: refactoring

--- pkg/playerCoin/exception/playerCoinShowing.go
+++ pkg/playerCoin/exception/playerCoinShowing.go
@@ -1,9 +1,7 @@
 package exception
 
-import "fmt"
-
 type PlayerCoinShowing struct{}
 
 func (e *PlayerCoinShowing) Error() string {
-	return fmt.Sprintf("player coin showing failed")
+	return "player coin showing failed"
 }
